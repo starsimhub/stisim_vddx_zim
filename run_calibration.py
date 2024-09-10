ad9b3bcec62ba586e65205640d57c1dba55cf4b9
Robyn Stuart <robyna.s@gmail.com>
2024-09-10 15:40:21 +0100

    trial run

--- run_calibration.py
+++ run_calibration.py
@@ -23,7 +23,7 @@ debug = False  # If True, this will do smaller runs that can be run locally for 
 do_save = True
 
 # Run settings for calibration (dependent on debug)
-n_trials = [5000, 10][debug]  # How many trials to run for calibration
+n_trials = [1000, 10][debug]  # How many trials to run for calibration
 n_workers = [40, 1][debug]  # How many cores to use
 storage = ["mysql://hpvsim_user@localhost/hpvsim_db", None][debug]  # Storage for calibrations
 
