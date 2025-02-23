cc277d9b9e12cf41da9e3c88620fa2bc19d56cb6
Robyn Stuart <robyna.s@gmail.com>
2025-02-24 10:49:44 +1100

    run more

--- plot_calibration.py
+++ plot_calibration.py
@@ -15,7 +15,7 @@ def plot_calibration(calib, start_year=2000, end_year=2025, res_to_plot=100):
     set_font(size=30)
     fig, axes = pl.subplots(3, 4, figsize=(20, 12))
     axes = axes.ravel()
-    disease_map = {'ng': 'Gonorrhea', 'ct': 'Chlamydia', 'tv': 'Trich'}  #, 'bv': 'Other'}
+    disease_map = {'ng': 'NG', 'ct': 'CT', 'tv': 'TV'}  #, 'bv': 'Other'}
     result_map = {
         'prevalence_f_15_25': 'Prevalence F 15-25',
         'prevalence': 'Prevalence',
@@ -31,7 +31,6 @@ def plot_calibration(calib, start_year=2000, end_year=2025, res_to_plot=100):
 
             # Pull out model results and data
             res = calib.sim_results[:res_to_plot]
-            target_data = calib.data[dname+'_'+resname]
             resname = dname+'_'+resname
 
             year = []
@@ -43,25 +42,26 @@ def plot_calibration(calib, start_year=2000, end_year=2025, res_to_plot=100):
                 seed += [run_num]*len(run['time'])
 
             modeldf = pd.DataFrame({'time': year, resname: values, 'seed': seed})
-            model_stats = modeldf.groupby(['time']).describe()
+            model_stats = modeldf.groupby(['time']).describe(percentiles=[.1, .9])
             model_plot = model_stats.iloc[(model_stats.index >= start_year) & (model_stats.index <= end_year)]
 
             x = model_plot.index
             medians = model_plot[(resname, '50%')]
-            quartiles1 = model_plot[(resname, '25%')]
-            quartiles3 = model_plot[(resname, '75%')]
+            quartiles1 = model_plot[(resname, '10%')]
+            quartiles3 = model_plot[(resname, '90%')]
 
             ax.plot(x, medians)
             ax.fill_between(x, quartiles1, quartiles3, alpha=0.3)
 
             # Plot data
-            if resname == 'prevalence_f_15_25':
+            if 'prevalence_f_15_25' in resname:
+                target_data = calib.data[resname]
                 data_plot = target_data.iloc[(target_data.index >= start_year) & (target_data.index <= end_year)]
                 xdata = data_plot.index
                 ydata = data_plot.values
                 ax.scatter(xdata, ydata, color='k', marker='s', label='Data')
 
-            ax.set_title(dlabel+reslabel)
+            ax.set_title(dlabel+' '+reslabel)
             ax.set_ylim(bottom=0)
             sc.SIticks(ax=ax)
 
