ad88b7d9200ae1ab2b8e23ac96a5bdf752fd0bfa
Robyn Stuart <robyna.s@gmail.com>
2025-02-26 08:05:24 +1100

    add fig5

--- run_syndromic_scens.py
+++ run_syndromic_scens.py
@@ -3,9 +3,12 @@ import pandas as pd
 import sciris as sc
 import starsim as ss
 import stisim as sti
+import matplotlib.pyplot as pl
+import seaborn as sns
 
 # From this repo
 from model import make_sim, make_scenpars
+from utils import set_font
 
 
 def load_calib_pars(scenario=None, calib=None, i=None):
@@ -58,16 +61,39 @@ def run_syndromic_scens(scenarios, stop=2040, parallel=True):
 
 
 def process_results(df):
-    res = pd.DataFrame()
-    for scen in ['treat100', 'treat80', 'treat50']:
+    healthdfs = sc.autolist()
+    treatdfs = sc.autolist()
+
+    tx_labels = {'ng_tx':'NG', 'ct_tx':'CT', 'metronidazole':'MTNZ'}
+
+    for scen in ['treat50', 'treat80', 'treat100']:
         for parset in df.parset.unique():
             thisdf = df.loc[(df.parset == parset) & (df.scenario.str.contains(scen))]
+
             for dis in ['ng', 'ct', 'tv']:
+                hres = pd.DataFrame()
+                hres['scenario'] = [scen]
+                hres['parset'] = [parset]
                 soc = thisdf.loc[(thisdf.poc == 0) & (thisdf.timevec > 2027)][dis+'.new_infections'].sum()
                 poc = thisdf.loc[(thisdf.poc == 1) & (thisdf.timevec > 2027)][dis+'.new_infections'].sum()
-                res[dis+'_infections'] = (soc - poc)/soc
+                hres['disease'] = [dis.upper()]
+                hres['infections'] = [(soc - poc)/soc*100]
+                healthdfs += hres
 
-    return res
+            for tx in ['ng_tx', 'ct_tx', 'metronidazole']:
+                tres = pd.DataFrame()
+                tres['scenario'] = [scen]
+                tres['parset'] = [parset]
+                soc = thisdf.loc[(thisdf.poc == 0) & (thisdf.timevec > 2027)][tx+'.new_treated_unnecessary_f'].sum()
+                poc = thisdf.loc[(thisdf.poc == 1) & (thisdf.timevec > 2027)][tx+'.new_treated_unnecessary_f'].sum()
+                tres['treatment'] = tx_labels[tx]
+                tres['overtreatments'] = [(soc - poc)/soc*100]
+                treatdfs += tres
+
+    healthdf = pd.concat(healthdfs)
+    treatdf = pd.concat(treatdfs)
+
+    return healthdf, treatdf
 
 
 if __name__ == '__main__':
@@ -78,8 +104,9 @@ if __name__ == '__main__':
     n_scen_runs = [10, 1][debug]  # Number of parameter sets to run per scenario
     scenarios = ['treat50', 'treat80', 'treat100']  #, 'panel']
     to_run = [
-        'run_syndromic_scens',
-        # 'process_results',
+        # 'run_syndromic_scens',
+        'process_results',
+        'plot_results',
     ]
 
     if 'run_syndromic_scens' in to_run:
@@ -90,7 +117,31 @@ if __name__ == '__main__':
     if 'process_results' in to_run:
         # Load
         df = sc.loadobj('results/synd_scens.obj')
-        res = process_results(df)
+        healthdf, treatdf = process_results(df)
+        sc.saveobj('results/synd_health.obj', healthdf)
+        sc.saveobj('results/synd_treat.obj', treatdf)
+
+    if 'plot_results' in to_run:
+        hdf = sc.loadobj('results/synd_health.obj')
+        tdf = sc.loadobj('results/synd_treat.obj')
+        set_font(size=20)
+        fig, axes = pl.subplots(1, 2, figsize=(20, 8))
+        axes = axes.ravel()
+
+        # Plot 1: Health
+        ax = axes[0]
+        sns.boxplot(data=hdf, x="disease", y="infections", hue="scenario", palette='viridis', ax=ax)
+        ax.set_title('% reduction in infections, 2027-2040')
+        ax.set_ylim(-10, 100)
+
+        # Plot 2: Treatment
+        ax = axes[1]
+        sns.boxplot(data=tdf, x="treatment", y="overtreatments", hue="scenario", palette='viridis', ax=ax)
+        ax.set_title('% reduction in overtreatment, 2027-2040')
+        ax.set_ylim(0, 100)
+
+        fig.tight_layout()
+        pl.savefig(f"figures/fig5_impact.png", dpi=100)
 
     print('Done!')
 
