28a1aede22b8dde3918a8a732a08a88aa7d50bf9
Robyn Stuart <robyna.s@gmail.com>
2025-04-04 05:48:17 +1100

    add durations

--- run_calibration.py
+++ run_calibration.py
@@ -38,19 +38,19 @@ def run_calibration(scenario, n_trials=None, n_workers=None, do_save=False):
             ng_p_symp=dict(low=0.15, high=0.2, guess=0.18),
             ct_p_symp=dict(low=0.25, high=0.3, guess=0.28),
             tv_p_symp=dict(low=0.5, high=0.75, guess=0.6),
-            p_symp_care=dict(low=0.45, high=0.75, guess=0.55),
+            p_symp_care=dict(low=0.35, high=0.75, guess=0.55),
         ),
         treat80=dict(
             ng_p_symp=dict(low=0.12, high=0.8, guess=0.15),
             ct_p_symp=dict(low=0.22, high=0.27, guess=0.25),
             tv_p_symp=dict(low=0.3, high=0.6, guess=0.45),
-            p_symp_care=dict(low=0.35, high=0.65, guess=0.45),
+            p_symp_care=dict(low=0.25, high=0.65, guess=0.45),
         ),
         treat100=dict(
             ng_p_symp=dict(low=0.1, high=0.5, guess=0.12),
             ct_p_symp=dict(low=0.2, high=0.25, guess=0.22),
             tv_p_symp=dict(low=0.15, high=0.5, guess=0.3),
-            p_symp_care=dict(low=0.15, high=0.45, guess=0.35),
+            p_symp_care=dict(low=0.15, high=0.55, guess=0.35),
         ),
     )
     calib_pars = calib_par_dict[scenario]
@@ -58,6 +58,8 @@ def run_calibration(scenario, n_trials=None, n_workers=None, do_save=False):
         ng_beta_m2f=dict(low=0.02, high=0.2, guess=0.05),
         ct_beta_m2f=dict(low=0.02, high=0.2, guess=0.05),
         tv_beta_m2f=dict(low=0.02, high=0.2, guess=0.05),
+        ng_dur=dict(low=6, high=10, guess=8, step=0.5),
+        ct_dur=dict(low=13, high=21, guess=15, step=0.5),
     )
     calib_pars = sc.mergedicts(calib_pars, beta_pars)
     # calib_pars = dict(
