87a26f0c1d6d4e83675726d63353d83f365d6885
Robyn Stuart <robyna.s@gmail.com>
2025-04-05 21:48:19 +1100

    75

--- run_calibration.py
+++ run_calibration.py
@@ -22,8 +22,8 @@ from model import make_sim, make_sim_pars
 
 # Run settings
 debug = False  # If True, this will do smaller runs that can be run locally for debugging
-n_trials = [8000, 2][debug]  # How many trials to run for calibration
-n_workers = [80, 1][debug]    # How many cores to use
+n_trials = [7500, 2][debug]  # How many trials to run for calibration
+n_workers = [75, 1][debug]    # How many cores to use
 # storage = ["mysql://hpvsim_user@localhost/hpvsim_db", None][debug]  # Storage for calibrations
 storage = None
 do_shrink = True  # Whether to shrink the calibration results
