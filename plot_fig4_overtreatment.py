6695283d0ac9a8957133badd07b2dfa07aa00a57
Robyn Stuart <robyna.s@gmail.com>
2025-04-24 07:24:41 +1000

    more

--- plot_fig4_overtreatment.py
+++ plot_fig4_overtreatment.py
@@ -10,15 +10,96 @@ import matplotlib.pyplot as pl
 import seaborn as sns
 import utils as ut
 
-if __name__ == '__main__':
 
-    show = True
+def plot_overtx(odf, hdf, tdf):
+    # Plot settings
+    ut.set_font(size=30)
+    fig, axes = pl.subplots(1, 2, figsize=(12, 5))
+    axes = axes.ravel()
+    legendfont = 16
 
-    # Load data
-    odf = sc.loadobj(f'results/overtx.obj')
-    hdf = sc.loadobj('results/synd_health.obj')
-    tdf = sc.loadobj('results/synd_treat.obj')
+    clist = sc.gridcolors(3)
+    colors = sc.objdict(treat50=clist[0], treat80=clist[1], treat100=clist[2])
 
+    # First row: reduction in overtreatment over time, by treatment type
+    t = odf.timevec.unique()
+    si = sc.findfirst(t, 2010)
+    ei = sc.findfirst(t, 2040)
+
+    for pn, (txname, txlabel) in enumerate(ut.tx_labels.items()):
+        if txname != 'metronidazole':
+            ax = axes[pn]
+            for scenario in ut.scenarios:
+                socdf = odf.loc[(odf.scenario == scenario) & (odf.treatment == txname) & (odf.variable == txname+'.new_treated_unnecessary_f')]
+                socy = socdf['value'][si:ei]
+                socy = socy.rolling(3, min_periods=1).mean()
+                ax.plot(t[si:ei], socy, label=ut.txscenlabels[scenario], color=colors[scenario])
+            for scenario in ut.scenarios:
+                pocdf = odf.loc[(odf.scenario == (scenario+'poc')) & (odf.treatment == txname) & (odf.variable == txname+'.new_treated_unnecessary_f')]
+                pocy = pocdf['value'][si:ei]
+                pocy = pocy.rolling(3, min_periods=1).mean()
+                ax.plot(t[si:ei], pocy, label=ut.txscenlabels[scenario], color=colors[scenario], ls='--')
+
+            if pn == 0:
+                h,l = ax.get_legend_handles_labels()  # #Get the legend handles and labels
+                l1 = ax.legend(h[:3], l[:3], loc='upper left', frameon=False, prop={'size': legendfont})
+                from matplotlib.lines import Line2D
+                myHandle = [Line2D([], [], ls='-', color='k'), Line2D([], [], ls='--', color='k')]
+                l2 = ax.legend(handles=myHandle, labels=['SOC', 'POC'], loc='lower left', bbox_to_anchor=(0, 0), frameon=False, prop={'size': legendfont})
+                ax.add_artist(l1)
+
+            ax.set_title(f'{txlabel} overtreatment')
+            ax.set_ylim(bottom=0)
+            sc.SIticks(ax)
+
+    fig.tight_layout()
+    pl.savefig(f"figures/fig4_slide_poctx.png", dpi=100)
+    if show:
+        pl.show()
+    return
+
+
+def plot_health(odf, hdf, tdf):
+    # Plot settings
+    ut.set_font(size=20)
+    fig, ax = pl.subplots(1, 1, figsize=(7, 4))
+    # axes = axes.ravel()
+    legendfont = 14
+
+    clist = sc.gridcolors(2)
+    colors = sc.objdict(treat50=clist[0], treat80=clist[1])
+
+    # # Cumulative reduction in overtreatment
+    # ax = axes[0]
+    # tdf.reset_index(inplace=True)
+    # toplot = tdf.loc[tdf.scenario != 'Treat-all']
+    # sns.boxplot(data=toplot, x="treatment", y="overtreatments", hue="scenario", palette=clist, ax=ax)
+    # ax.set_title('% reduction in overtreatment, 2027-2040')
+    # ax.set_ylim(0, 100)
+    # ax.get_legend().set_visible(False)
+    # ax.set_xlabel('')
+    # ax.set_ylabel('')
+    #
+    # Second row, plot 2: Cumulative reduction in infections
+    # ax = axes[0]
+    hdf.reset_index(inplace=True)
+    toploth = hdf.loc[hdf.scenario != 'Treat-all']
+    sns.boxplot(data=toploth, x="disease", y="infections", hue="scenario", palette=clist, ax=ax)
+    ax.legend(frameon=False, prop={'size': legendfont})
+    ax.set_title('% reduction in infections, 2027-2040')
+    ax.set_ylim(0, 100)
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+
+    fig.tight_layout()
+    pl.savefig(f"figures/fig4_impact_slide.png", dpi=100)
+    if show:
+        pl.show()
+    return
+
+
+
+def plot_fig4(odf, hdf, tdf):
     # Plot settings
     ut.set_font(size=30)
     fig = pl.figure(figsize=(20, 16))
@@ -84,6 +165,22 @@ if __name__ == '__main__':
     pl.savefig(f"figures/fig4_poctx.png", dpi=100)
     if show:
         pl.show()
+    return
+
+
+if __name__ == '__main__':
+
+    show = False
+
+    # Load data
+    odf = sc.loadobj(f'results/overtx.obj')
+    hdf = sc.loadobj('results/synd_health.obj')
+    tdf = sc.loadobj('results/synd_treat.obj')
+
+    # Plot overtreatment
+    plot_overtx(odf, hdf, tdf)
+    # Plot health impact
+    plot_health(odf, hdf, tdf)
 
     print('Done!')
 
