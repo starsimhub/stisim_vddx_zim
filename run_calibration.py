19069b802f0f5971768e0c329c72896a2054a80a
Robyn Stuart <robyna.s@gmail.com>
2025-05-01 12:43:54 +1000

    rerun calibration for ct in 25f

--- run_calibration.py
+++ run_calibration.py
@@ -21,7 +21,7 @@ from model import make_sim, make_sim_pars
 
 # Run settings
 debug = False  # If True, this will do smaller runs that can be run locally for debugging
-n_trials = [1200, 2][debug]  # How many trials to run for calibration
+n_trials = [800, 2][debug]  # How many trials to run for calibration
 n_workers = [80, 1][debug]    # How many cores to use
 storage = None
 do_shrink = True  # Whether to shrink the calibration results
