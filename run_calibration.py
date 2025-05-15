9850b570c035aeafd99ad2c5156c2d40dd57b7ba
Robyn Stuart <robyna.s@gmail.com>
2025-05-15 10:55:35 +1000

    singe scen

--- run_calibration.py
+++ run_calibration.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     load_partial = True
 
     # Loop over scenarios and run calibrations for each
-    for scenario in ut.scenarios:
+    for scenario in ['treat50']: #ut.scenarios:
 
         sc.heading(f'Running calibration: {scenario}')
         sim, calib = make_calibration(scenario, n_trials=n_trials, n_workers=n_workers, constrain=constrain)
